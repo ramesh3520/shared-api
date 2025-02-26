9bf88fcd84cd7c4a49636f689b7c930dae61bc70
ramesh3520 <rameshkumardbg011@gmail.com>
2025-02-26 10:21:14 +0530

    add shared api

---
+++ webStorm.config.js
@@ -0,0 +1,11 @@
+System.config({
+    paths: {
+        '@enums': 'src/enums',
+        '@http-response': 'src/http-response',
+        '@mailer': 'src/mailer',
+        '@models': 'src/models',
+        '@schemas': 'src/schemas',
+        '@sms': 'src/sms',
+        '@utils': 'src/utils',
+    },
+});
